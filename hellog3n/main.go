9800a83d38bbb662159d6a476bb3a112b7a08aa9
Cove Schneider <covecs@gmail.com>
2018-12-17 23:09:01 -0800

    add debugging

--- hellog3n/main.go
+++ hellog3n/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
+	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/util/application"
-	"github.com/g3n/engine/geometry"
-	"github.com/g3n/engine/material"
-	"github.com/g3n/engine/math32"
-	"github.com/g3n/engine/graphic"
-	"github.com/g3n/engine/light"
 )
 
 func main() {
@@ -17,23 +13,10 @@ func main() {
 		Height: 600,
 	})
 
-	// Create a blue torus and add it to the scene
-	geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
-	mat := material.NewPhong(math32.NewColor("DarkBlue"))
-	torusMesh := graphic.NewMesh(geom, mat)
-	app.Scene().Add(torusMesh)
+	w1 := gui.NewWindow(100,100)
+	w1.SetPosition(100,100)
+	w1.SetResizable(true)
+	app.Gui().Add(w1)
 
-	// Add lights to the scene
-	ambientLight := light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8)
-	app.Scene().Add(ambientLight)
-	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
-	pointLight.SetPosition(1, 0, 2)
-	app.Scene().Add(pointLight)
-
-	// Add an axis helper to the scene
-	axis := graphic.NewAxisHelper(0.5)
-	app.Scene().Add(axis)
-
-	app.CameraPersp().SetPosition(0, 0, 3)
 	app.Run()
 }
